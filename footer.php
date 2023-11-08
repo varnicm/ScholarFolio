50d4f9b16917df4259587d6b380c8895f9356d6f
Moe Sanjari <moe.sanjari@gmail.com>
2023-11-07 22:31:43 -0600

    adjust footer

--- footer.php
+++ footer.php
@@ -2,15 +2,15 @@
     <div class="container ">
         <div class="row">
             
-  <div class="col-md-4 contact-info">
-    <h3>Contact Information</h3>
+  <div class="col-md-2 contact-info">
+    <h3>Contact Information</h4>
     <p>[Your Full Name]</p>
     <p><a href="mailto:[Your Email]">[Your Email]</a></p>
     <!-- Additional contact details -->
   </div>
   
-  <div class="col-md-4 quick-links">
-    <h3>Quick Links</h3>
+  <div class="col-md-2 quick-links">
+    <h4>Quick Links</h4>
     <ul>
       <li><a href="#about">About Me</a></li>
       <li><a href="#research">Research</a></li>
@@ -19,24 +19,24 @@
     </ul>
   </div>
   
-  <div class="col-md-4 professional-affiliations">
-    <h3>Professional Affiliations</h3>
+  <div class="col-md-2 professional-affiliations">
+    <h4>Professional Affiliations</h4>
     <!-- List of affiliations -->
   </div>
   
   
   
-  <div class="col-md-4 social-media">
-    <h3>Connect with Me</h3>
+  <div class="col-md-2 social-media">
+    <h4>Connect with Me</h4>
     <!-- Social Media Links -->
   </div>
   
-  <div class="col-md-4 newsletter-signup">
-    <h3>Stay Updated</h3>
+  <div class="col-md-2 newsletter-signup">
+    <h4>Stay Updated</h4>
     <!-- Newsletter Form -->
   </div>
   
-  <div class="col-md-4 acknowledgements">
+  <div class="col-md-2 acknowledgements">
   <h3>Acknowledgements</h3>
     <!-- Acknowledgements -->
   </div>
