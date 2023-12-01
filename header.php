819b29a3fb3a4f963777d5e1e71f0c5f116315c4
Moe Sanjari <sanjari@gmail.com>
2023-11-30 18:42:31 -0600

    some changes on links and header

--- header.php
+++ header.php
@@ -39,7 +39,6 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand" href="#">Navbar</a>
                 <?php
                 wp_nav_menu( array(
                     'theme_location'    => 'primary',
